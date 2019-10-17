905e5c8694cf0bc7f9fe38285ff7e8e7af3ce317
Roy Nunez <roynunez273@gmail.com>
2019-10-17 08:43:54 -0400

    Pizza and Customer model

--- PizzaBrosRESTAPI/PizzaBro-REST-Backend/src/main/java/com/PizzaBros/model/Pizza.java
+++ PizzaBrosRESTAPI/PizzaBro-REST-Backend/src/main/java/com/PizzaBros/model/Pizza.java
@@ -1,5 +1,80 @@
 package com.PizzaBros.model;
 
-public class Pizza {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.transaction.Transactional;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import org.springframework.stereotype.Service;
+
+@Entity
+@Table(name ="Pizza")
+public class Pizza implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id");
+	private Long id;
+	private String name;
+	private String description;
+	private String image;
+	private Double price;
+	private Integer inventory;
+	private Integer sales;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Integer getInventory() {
+		return inventory;
+	}
+	public void setInventory(Integer inventory) {
+		this.inventory = inventory;
+	}
+	public Integer getSales() {
+		return sales;
+	}
+	public void setSales(Integer sales) {
+		this.sales = sales;
+	}
+	
+	
+	
 }
+
+
+
+
+
