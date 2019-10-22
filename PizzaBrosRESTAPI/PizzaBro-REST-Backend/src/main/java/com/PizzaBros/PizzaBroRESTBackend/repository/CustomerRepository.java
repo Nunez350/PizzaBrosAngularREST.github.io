c3e1aae85965edb23ed5839086634a0adb9b3145
Roy Nunez <roynunez273@gmail.com>
2019-10-22 14:19:07 -0400

    adddint terry's customer stuff

---
+++ PizzaBrosRESTAPI/PizzaBro-REST-Backend/src/main/java/com/PizzaBros/PizzaBroRESTBackend/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+
+package com.PizzaBros.PizzaBroRESTBackend.repository;
+
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.PizzaBros.PizzaBroRESTBackend.model.Customer;
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String>{
+    List<Customer> findAllById(String customerId );
+    List<Customer> findAll();
+}
