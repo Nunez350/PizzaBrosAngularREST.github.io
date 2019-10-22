e8e05ac4d9a206dc934c65493fa1343b5ff19b3c
qbieber <qbieber@hotmail.com>
2019-10-22 14:03:37 -0400

    Fixed Front End Thanks Reynald!

--- AngPizzaStore/src/app/models/model.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Repository } from './repository';
-import { Cart } from './cart.model';
-import { Order } from './order.module';
-
-@NgModule({
-  providers: [Repository, Cart, Order]
-})
-export class ModelModule { }
