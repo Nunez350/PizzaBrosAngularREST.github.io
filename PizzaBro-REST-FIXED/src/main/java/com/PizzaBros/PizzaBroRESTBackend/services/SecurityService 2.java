7b6574553663e7c95c72678c5177e03f8b1525bc
Roy Nunez <roynunez273@gmail.com>
2019-10-30 09:58:15 -0400

    Saving work on auth

--- PizzaBro-REST-FIXED/src/main/java/com/PizzaBros/PizzaBroRESTBackend/services/SecurityService 2.java
+++
@@ -1,7 +0,0 @@
-package com.PizzaBros.PizzaBroRESTBackend.services;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-
-    void autoLogin(String username, String password);
-}
